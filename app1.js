df386d68825b35a5645f0653b04e0b5c169863a6
dhirajhack6 <159863667+dhirajhack6@users.noreply.github.com>
2025-11-26 00:26:30 +0530

    first commit

---
+++ app1.js
@@ -0,0 +1,113 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["yellow", "red", "purple", "green"];
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress", function () {
+  if (started == false) {
+    console.log("Game Started");
+    started = true;
+
+    levelUp();
+  }
+});
+
+function gameFlash(btn) {
+  btn.classList.add("flash");
+  setTimeout(function () {
+    btn.classList.remove("flash");
+  }, 250);
+}
+
+function userFlash(btn) {
+  btn.classList.add("userflash");
+  setTimeout(function () {
+    btn.classList.remove("userflash");
+  }, 250);
+}
+function levelUp() {
+  userSeq = [];
+  level++;
+  h2.innerText = `Level ${level}`;
+
+  let randIdx = Math.floor(Math.random() * btns.length);
+  let randColor = btns[randIdx];
+  let randBtn = document.querySelector(`.${randColor}`);
+  gameSeq.push(randColor);
+  console.log(gameSeq);
+  gameFlash(randBtn);
+}
+
+function checkAns(idx) {
+  if (userSeq[idx] === gameSeq[idx]) {
+      if (userSeq.length === gameSeq.length) {
+        updateScore(1);
+        setTimeout(levelUp, 1000);
+        
+    }
+  } else {
+    h2.innerHTML = `Game Over!</b> <br>
+         Press any key to start`;
+      resetScore();
+      
+    document.querySelector("body").style.backgroundColor = "red";
+    setTimeout(function () {
+      document.querySelector("body").style.backgroundColor = "white";
+    }, 150);
+    reset();
+    started = false;
+    level = 0;
+    gameSeq = [];
+  }
+}
+
+
+function btnPress() {
+  let btn = this;
+  userFlash(btn);
+
+  let userColor = btn.getAttribute("id");
+  userSeq.push(userColor);
+
+  checkAns(userSeq.length - 1);
+}
+
+let allBtn = document.querySelectorAll(".btn");
+for (let btn of allBtn) {
+  btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+  started = false;
+  gameSeq = [];
+  userSeq = [];
+  level = 0;
+}
+
+const scoreE1 = document.getElementById("score");
+const highScoreE1 = document.getElementById("high-score");
+
+let currentScore = 0;
+let highScore = 0;
+
+function updateScore(points) {
+    currentScore += points; 
+    scoreE1.textContent = currentScore;
+
+    // Update high score if beaten
+    if (currentScore > highScore) {
+        highScore = currentScore;
+        highScoreE1.textContent = highScore;
+    }
+}
+
+function resetScore() {
+    currentScore = 0;
+    scoreE1.textContent = currentScore;
+}
+
+
